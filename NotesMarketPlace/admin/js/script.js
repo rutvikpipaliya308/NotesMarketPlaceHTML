e89ef4b6ba892bb1680df6586ce667730a5e8178
rutvik <rmpipaliya308@gmail.com>
2021-01-11 18:33:39 +0530

    added notesmarketplace

---
+++ NotesMarketPlace/admin/js/script.js
@@ -0,0 +1,20 @@
+/* =============== Mobile Navigation ============== */
+$(function () {
+	$('.menu-toggle').click(function () {
+		// show and hide Mobile Toggle Btn
+		$(".nav-menu-link").toggleClass("mobile-nav");
+		$(this).toggleClass("is-active");
+	});
+});
+
+/* ================= password show/hide =============== */
+$(document).ready(function () {
+	$("body").on('click', '.toggle-password', function () {
+		var input = $($(this).attr("toggle"));
+		if (input.attr("type") === "password") {
+			input.attr("type", "text");
+		} else {
+			input.attr("type", "password");
+		}
+	});
+});
